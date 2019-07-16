e602d0e1d3459979f0e05bc9eacac204598e6504
sangram203 <sangram.hati@gmail.com>
2019-07-16 12:47:24 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, LoginComponent, HomeComponent, AdminComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent },
+      {path:'/login', component: LoginComponent },
+      {path:'/admin', component: AdminComponent}
+    ]) 
+    ],
+  declarations: [ 
+    AppComponent, 
+    LoginComponent, 
+    HomeComponent, 
+    AdminComponent 
+    ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
