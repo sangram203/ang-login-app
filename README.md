bfd2f15ca49b7ff2aa3cfaf20c57a8195ee6c10a
sangram203 <sangram.hati@gmail.com>
2019-07-16 11:40:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ts351o
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ts351o)
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:'app-form',
+  templateUrl:'./login.component.html',
+  styleUrls:['./login.component.css']
+})
+export class LoginComponent{
+  username="sangram.hati@mphasis.com"
+}
