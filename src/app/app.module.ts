656bf2567a4bce8b49c0f664a42196bc7910093d
sangram203 <sangram.hati@gmail.com>
2019-07-16 14:50:36 +0530

    3 files modified, 9 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   imports:      [ 
@@ -15,15 +16,17 @@ import { AdminComponent } from './admin/admin.component';
     HttpClientModule,
     RouterModule.forRoot([
       {path:'', component: HomeComponent },
-      {path:'/login', component: LoginComponent },
-      {path:'/admin', component: AdminComponent}
+      {path:'login', component: LoginComponent },
+      {path:'admin', component: AdminComponent},
+      {path:'**',component: NotFoundComponent}
     ]) 
     ],
   declarations: [ 
     AppComponent, 
     LoginComponent, 
     HomeComponent, 
-    AdminComponent 
+    AdminComponent,
+    NotFoundComponent 
     ],
   bootstrap:    [ AppComponent ]
 })
